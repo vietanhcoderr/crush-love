93f0d10385ff856b7a8f7259519ea95b10184ef5
vietanhcoderr <162428506+vietanhcoderr@users.noreply.github.com>
2025-03-07 23:40:17 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào người con gái anh yêu, anh có điều muốn nói. </br> Vẫn tiếp tục trên hành trình tình yêu của hai đứa nha!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+    "Hãy trả lời một cách thật lòng nhé. Nếu em im lặng và rời đi thì tức là em đã ĐỒNG Ý.",
+  buttonYes: "Em đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Cảm ơn em nhìu nhìu.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn em đã cho anh một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ em. </br> anh biết anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim đủ chân thành. Em chính là cả tất cả của anh. </br> Một ngày không xa đâu anh sẽ đến đón em. Em mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho em đó.</br> Love you <3",
 };
